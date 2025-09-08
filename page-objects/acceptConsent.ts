2ec8766e4004d9a1da2432623ec94b97db5c2cbe
Oleh <56585458+Oleh88777@users.noreply.github.com>
2025-09-08 21:43:56 +0200

    createt test wrong email or password

--- page-objects/acceptConsent.ts
+++ page-objects/acceptConsent.ts
@@ -27,5 +27,23 @@ export class singupAcceptConsent extends HelperBase{
       await expect(h2newUserSignUp).toBeVisible();
   }
 
+
+  async acceptConsentWrongloginEmail () {
+     const consentButton = this.page.getByRole('button', { name: 'Consent' });
+    if (await consentButton.isVisible()) {
+      await consentButton.click();
+    }
+
+      const responsePage = await this.page.request.get('https://automationexercise.com/');
+      expect(responsePage.status()).toBe(200);
+
+      const loginLink = this.page.locator('a[href="/login"]');
+      await expect(loginLink).toBeVisible();
+      await loginLink.click();
+
+      const h2newUserSignUp = this.page.getByRole('heading', { name: 'New User Signup!' });
+      await expect(h2newUserSignUp).toBeVisible();
+  }
+
 }
  
