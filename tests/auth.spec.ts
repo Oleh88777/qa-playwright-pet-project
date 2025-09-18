9b34174cacb02424e595ded820aeaee40ba4db4d
Oleh <56585458+Oleh88777@users.noreply.github.com>
2025-09-18 22:15:22 +0200

    select date of birth options test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -2,12 +2,10 @@ import {test, expect} from '@playwright/test'
 import {MainNavigationBar} from '../page-objects/navigationPage';
 import { AcceptConsent } from '../page-objects/acceptConsent';
 import { AuthLoginSignup } from '../page-objects/authPage';
-import { PassThrough } from 'stream';
 
 let name = 'Oleh';
 let email = 'mykhayliv8877@gmail.com';
 let password  = 'Europe2025$';
-
 
 test.describe('Auth flow cases 1-5', () => {
   let nav: MainNavigationBar
@@ -20,7 +18,6 @@ test.describe('Auth flow cases 1-5', () => {
       
         await nav.openHomePage();
         await consent.acceptConsents();
- 
 })
 
 test('Register user case:1', async ({page})=> {
@@ -34,6 +31,16 @@ let title = page.getByRole('heading', {name: 'Enter Account Information'});
 await expect(title).toBeVisible();
 
 await loginSignUp.inputPassowrd(password);
+
+const selectBirthBay = page.locator('#days');
+await selectBirthBay.selectOption('31');
+await expect(selectBirthBay).toHaveValue('31');
+
+const selectoMonth = page.locator('#months');
+await selectoMonth.selectOption('3');
+
+const selectbirthYear = page.locator('#years');
+await selectbirthYear.selectOption('1994');
 })
 
 });
