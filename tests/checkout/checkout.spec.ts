d2c805620c28903f9349059a8fcf6106ea20949d
Oleh <mykhayliv88777@gmail.com>
2025-10-10 21:54:56 +0200

    verefiing card deteils and quantity continue with test Order

--- tests/checkout/checkout.spec.ts
+++ tests/checkout/checkout.spec.ts
@@ -27,6 +27,37 @@ test('Perform an Order ', async ({ page }) => {
   await buttonAddToCard.click();
   await consent.continueShopping();
   await nav.buttonCard();
+
+  const ProceedToChekcout = page.getByText("Proceed To Checkout");
+  await expect(ProceedToChekcout).toBeVisible();
+  await ProceedToChekcout.click();
+
+  
+const listItems = page.locator('#address_delivery li');
+await expect(listItems).toHaveText([
+  'Your delivery address',
+  'Mr. Oleh Mykhayliv',
+  'MyCompany',
+  'Street 1',
+  'Office 2',
+  'Lviv Lviv 01001',
+  'Ukraine',
+  '+380501234567'
+]);
+
+
+const ordrQuantity = page.getByRole('button', {name: /\d+/});
+const text = await ordrQuantity.textContent();
+console.log(text, 'texy');
+
+const numberText = Number(text);
+console.log(numberText);
+
+if (numberText > 2 ) {
+   await nav.buttonCard();
+   const deleteQInCard = page.locator('.cart_quantity_delete');
+   await deleteQInCard.click();
+}
 }); 
 
     
